4fcfa0057318c1f4437f9dfac49f681648435632
Jeff Kolesky <jeff@kolesky.com>
2011-01-27 16:53:08 -0500

    handles tagging of unnecessary tasks

--- src/main/scala/timetracker/model/Task.scala
+++ src/main/scala/timetracker/model/Task.scala
@@ -1,11 +1,29 @@
 package com.kolesky.timetracker.model
 
 // LERNIN: Should this be a case class?
+// LERNIN: If rawCategory is declared as a val here, is it exposed as a property of Task, and if so
+// how can I prevent it from being read by the outside world?  It should never be referenced once
+// the Task has been constructed.
 class Task(val day: Day, val duration: Int, val rawCategory: String, val description: String) {
-  val repeat = rawCategory.endsWith("*")
-  val category = if (repeat) { rawCategory.dropRight(1) } else { rawCategory }
+  val (category, repeat, unnecessary) = Task.parseCategory(rawCategory)
 
   override def toString = {
     format("   Task: %s; %s; %s", duration, category, description)
+  }
+}
+
+object Task {
+  // TODO: This is a poor design.  This parsing logic should live elsewhere and a Task should just
+  // take the final raw values.
+  private val tagRegex = "([^*-]+)([*-]+)$".r
+  private def parseCategory(category: String): (String, Boolean, Boolean) = {
+    tagRegex.findFirstMatchIn(category) match {
+      case Some(groups) => {
+        return (groups.group(1), groups.group(2).contains("*"), groups.group(2).contains("-"))
+      }
+      case None => {
+        return (category, false, false)
+      }
+    }
   }
 }
